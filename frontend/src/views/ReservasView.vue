18f1dd6b0d39c41f77caa9ce8c95fcb065194f11
Aintzane Goffard <aintzagoffard@gmail.com>
2025-07-30 19:29:23 +0200

    precio por persona

--- frontend/src/views/ReservasView.vue
+++ frontend/src/views/ReservasView.vue
@@ -26,7 +26,7 @@
           <p>{{ actividad.descripcion1 }}</p>
           <p><strong>Fecha:</strong> {{ actividad.fecha }}</p>
           <p><strong>Hora:</strong> {{ actividad.hora }}</p>
-          <p><strong>Precio:</strong> {{ actividad.precio }} €</p>
+          <p><strong>Precio:</strong> {{ actividad.precio }} € por persona</p>
         </div>
       </div>
     </div>
